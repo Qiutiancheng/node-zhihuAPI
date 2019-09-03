e4fc78fdb417e5ac7c9cd6ec0e5d112fce131435
cho <396163748@qq.com>
2019-09-03 23:17:42 +0800

    koa-router

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
+// 添加 /users 的路由前缀
+const usersRouter = new Router({prefix:'/users'})
+// 鉴权（demo）
+const auth = async (ctx,next)=>{
+    if(ctx.url !== '/users'){
+        ctx.throw(401)
+    }
+    await next()
+    
+}
 
-app.use(async (ctx)=>{ 
-    if(ctx.url === '/'){
-        ctx.body = 'This is home page.'
-    }
-    else if(ctx.url === '/users'){
-        if(ctx.method === 'GET'){
-            ctx.body = 'This is users page  '
-        }
-        else if(ctx.method === 'POST') {
-            ctx.body = 'This is page of creat user'
-        }
-        else {
-            ctx.status = 405
-        } 
-    }
-    else if(ctx.url.match(/\/users\/\w{12,12}/)){
-        const userId = ctx.url.match(/\/users\/(\w{12,12})/)[1]
-        ctx.body = `这是用户 ${userId}`
-    }
-    else {
-        ctx.status = 404
-    }
-}) 
+router.get('/', (ctx) => {
+    ctx.body = '这是主页'
+});
 
+// 会在响应前执行auth鉴权
+usersRouter.get('/',auth,(ctx)=>{
+    ctx.body = '这是用户列表'
+})
+
+usersRouter.post('/',auth,(ctx)=>{
+    ctx.body = '创建用户' 
+})
+
+usersRouter.get('/:id',auth,(ctx)=>{
+    const {id} = ctx.params
+    ctx.body = `这是用户：${id}`
+})
+
+// 任何中间件都需要使用use注册到app里
+app.use(router.routes())
+app.use(usersRouter.routes())
+// 返回允许的http方法；如果请求方法符合koa-router所支持的方法但是路由目前不存在此方法，会自动提示405
+app.use(usersRouter.allowedMethods())
 app.listen(8080)
