4b1928c6462feddcc4dba9073f9c3d10210f110d
cho <396163748@qq.com>
2019-09-07 22:19:35 +0800

    use koa-json-error

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,20 +3,47 @@ const db = [{name:'李雷'}]
 
 class UserCtl {
     getList(ctx){
+        a.b
         ctx.body = db
     }
     getUser(ctx){
+        if(ctx.params.id * 1 >= db.length){
+            ctx.throw(412)
+        }
         ctx.body = db[ctx.params.id*1]
     }
     create(ctx){
+        ctx.verifyParams({
+            name: {
+                type: 'string',
+                required: true
+            },
+            age: {
+                type: 'number',
+                required: false
+            }
+        })
         db.push(ctx.request.body)
-        ctx.body = 204
+        ctx.body = ctx.request.body
     }
     update(ctx){
+        ctx.verifyParams({
+            name: {
+                type: 'string',
+                required: true
+            },
+            age: {
+                type: 'number',
+                required: false
+            }
+        })
         db[ctx.params.id*1] = ctx.request.body
         ctx.body = 204
     }
     delete(ctx){
+        if(ctx.params.id * 1 >= db.length){
+            ctx.throw(412)
+        }
         db.splice(ctx.params.id*1,1)
         ctx.body = 204
     }
--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 const Koa = require('koa')
 // Koa无法解析请求体，需要此中间件才能解析
 const bodyparser = require('koa-bodyparser') 
+// 使用第三方错误处理中间件
+const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const app = new Koa()  
 const routing = require('./routes')
 const port = 3000
  
 
-
+app.use(error({
+    // 生产环境才现实错误的堆栈信息
+    postFormat:(e,{ stack, ...rest }) => process.env.NODE_ENV === 'production' ? rest: {stack, ...rest}
+}));
 // 任何中间件都需要使用use注册到app里
 app.use(bodyparser())
+app.use(parameter(app))
 routing(app)
+
 app.listen(port,()=>console.log(`程序启动在 ${port} 端口...`))
