0999da128a6fc014c8c1ce708f007ff155b73d37
cho <396163748@qq.com>
2019-09-09 23:23:30 +0800

    use koa-jwt

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,4 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jwt = require('koa-jwt')
 const Router = require('koa-router')
 const router = new Router({prefix:'/users'})
 const {
@@ -13,17 +13,7 @@ const {
 // 密钥
 const { secret } = require('../config')
 // 认证
-const auth = async(ctx, next)=>{
-    const { authorization = '' } = ctx.request.header
-    const token = authorization.replace('Bearer ','')
-    try {
-        const user = jsonwebtoken.verify(token,secret)  
-        ctx.state.user = user
-    } catch (error) {
-        ctx.throw(401, error.message)
-    }
-    await next()
-}
+const auth = jwt({secret})
 
 // 获取用户列表
 router.get('/', getList)
